1e129e5b1edc35d267e8c3437d0c3ab1c7ca6712
sgmikami03 <sgmikami03@gmail.com>
2023-07-20 08:18:14 +0900

    [add]404-style追加

--- 404.php
+++ 404.php
@@ -1,9 +1,9 @@
 <?php get_header(); ?>
 
-<main>
+<main class="main_404">
     <section class="content-404">
         <h2 class="content-404__title">404 Page Not Found</h2>
-        <p class="content-404__text">指定されたページは<br class="br-sp">見つかりませんでした</p>
+        <p class="content-404__text">指定されたページは見つかりませんでした</p>
     </section>
 </main>
 
